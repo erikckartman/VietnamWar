335ace3f8b59b073ec54cc5147fd1a5dc31ccdc6
erikckartman <severyukov05d@gmail.com>
2024-12-06 17:45:33 +0100

    BBBBBB

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,19 +8,9 @@ public class GameController : MonoBehaviour
     private float moveSpeed = 5f;
     private float lookSensitivity = 2f;
 
-    // Aiming
+    //Looking
     [SerializeField] private Transform cameraTransform;
     private float xRotation = 0f;
-
-    // Shooting
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform dulo;
-    private int bullets = 10;
-    [SerializeField] private float fireRate = 0.1f; 
-    private bool canShoot = true;
-
-    // Recoil
-    [SerializeField] private float recoilAmount = 3f; 
 
     private void Start()
     {
@@ -32,16 +22,6 @@ public class GameController : MonoBehaviour
     {
         MovePlayer();
         RotateView();
-
-        if (Input.GetMouseButton(0) && bullets > 0 && canShoot)
-        {
-            StartCoroutine(Shoot());
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            bullets = 10;
-        }
     }
 
     private void MovePlayer()
@@ -63,23 +43,5 @@ public class GameController : MonoBehaviour
 
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
-    }
-
-    private IEnumerator Shoot()
-    {
-        canShoot = false; 
-        bullets--;
-
-        GameObject bulletClone = Instantiate(bullet, dulo.position, dulo.rotation);
-        Rigidbody bulletRB = bulletClone.GetComponent<Rigidbody>();
-        bulletRB.velocity = dulo.forward * 50f;
-        Destroy(bulletClone, 0.5f);
-
-        xRotation -= recoilAmount;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        yield return new WaitForSeconds(fireRate);
-        canShoot = true;
-    }
+   } 
 }
