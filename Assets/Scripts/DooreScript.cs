618a016db3dbb5644c995abf6a3d229121285f01
erikckartman <cloverfield10lane@gmail.com>
2025-01-21 23:10:18 +0100

    Door interaction

---
+++ Assets/Scripts/DooreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DooreScript : MonoBehaviour
+{
+    [SerializeField] private float openAngle = 90f;
+    [SerializeField] private float openingSpeed = 2f;
+
+    private bool isOpen = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    private void Start()
+    {
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, isOpen ? openRotation : closedRotation, Time.deltaTime * openingSpeed);
+    }
+
+    public void OpenCloseDoor()
+    {
+        isOpen = !isOpen;
+    }
+}
