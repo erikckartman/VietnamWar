e59a6bcacf620f454cc35de76330e12446e9df5a
erikckartman <cloverfield10lane@gmail.com>
2025-03-30 21:25:17 +0200

    Safe door animation

--- Assets/Scripts/SafeMechanics/SafeIsOpened.cs
+++ Assets/Scripts/SafeMechanics/SafeIsOpened.cs
@@ -6,9 +6,26 @@ public class SafeIsOpened : MonoBehaviour
 {
     [SerializeField] private List<Animator> animator = new List<Animator>();
     [SerializeField] private GameObject objectToTake;
+    private bool isOpening = false;
+    private Quaternion targetRotation;
+    [SerializeField] private float openSpeed = 5f;
+    [SerializeField] private GameObject safeDoor;
+    private void Update()
+    {
+        if(!isOpening) return;
+
+        safeDoor.transform.rotation = Quaternion.Lerp(safeDoor.transform.rotation, targetRotation, Time.deltaTime * openSpeed);
+
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+        {
+            isOpening = false;
+        }
+    }
     public void OpenSafe()
     {
+        targetRotation = Quaternion.Euler(safeDoor.transform.rotation.eulerAngles.x, safeDoor.transform.rotation.eulerAngles.y + 135, safeDoor.transform.rotation.eulerAngles.z);
         objectToTake.GetComponent<Collider>().enabled = true;
         objectToTake.GetComponent<Rigidbody>().useGravity = true;
+        isOpening = true;
     }
 }
